e6439b4e8ec64c514b7a2c9bd32acf2453a346af
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-07-01 17:33:27 +0300

    feat: task-030 complete

---
+++ codewars/8kyu/029_Convert a Boolean to a String/script.ts
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+const booleanToString = (b: boolean): string => String(b);
+
+console.log(booleanToString(true));
