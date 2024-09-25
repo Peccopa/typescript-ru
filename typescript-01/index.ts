c68a812f806ce7f1f909772a9d269a4cca344d1f
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-09-25 11:28:34 +0300

    lesson: add typescript-01 index-009

--- typescript-01/index.ts
+++ typescript-01/index.ts
@@ -1,25 +1,28 @@
-const isBirthdayData: boolean = true;
-const userNameData: string = 'John';
-const ageData: number = 40;
+// const isBirthdayData: boolean = true;
+// const userNameData: string = 'John';
+// const ageData: number = 40;
 
-// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
-//   if (isBirthday) {
-//     return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
-//   } else {
-//     return 'Error ';
-//   }
-// }
+let salary: number;
+salary = 5000;
 
-const logBrtMsg = (
-  isBirthday: boolean,
-  userName: string,
-  age: number
-): string => {
+const userData: string =
+  '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
+
+const userObj: {
+  isBirthdayData: boolean;
+  userNameData: string;
+  ageData: number;
+} = JSON.parse(userData);
+console.log(userObj);
+
+function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
   if (isBirthday) {
     return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
   } else {
     return 'Error ';
   }
-};
+}
 
-console.log(logBrtMsg(isBirthdayData, userNameData, ageData));
+console.log(
+  logBrtMsg(userObj.isBirthdayData, userObj.userNameData, userObj.ageData)
+);
