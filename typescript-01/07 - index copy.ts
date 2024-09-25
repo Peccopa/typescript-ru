027f5d4d1213fd685eab4b6895d7235f556a2b7c
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-09-25 09:42:24 +0300

    fix: add ts-01 lesson-07

---
+++ typescript-01/07 - index copy.ts
@@ -0,0 +1,9 @@
+// let userName2: string = 'Ivan';
+
+// const isBirthday: boolean = true;
+// const age2: number = 40;
+// const userName2: string = 'John';
+
+// if (isBirthday) {
+//   console.log(`Congrats ${userName2.toUpperCase()}, age ${age2 + 1}`);
+// }
