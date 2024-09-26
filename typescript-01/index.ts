7b571983a97a65cd24903aa5ddc36707c0ba3673
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-09-26 07:03:15 +0300

    lesson: add typescript-01 index-011

--- typescript-01/index.ts
+++ typescript-01/index.ts
@@ -1,28 +1,18 @@
-// const isBirthdayData: boolean = true;
-// const userNameData: string = 'John';
-// const ageData: number = 40;
+const isBirthdayData: boolean = true;
+const ageData: number = 40;
+const userNameData: string = 'John';
 
-let salary: number;
-salary = 5000;
-
-const userData: string =
-  '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
-
-const userObj: {
-  isBirthdayData: boolean;
-  userNameData: string;
-  ageData: number;
-} = JSON.parse(userData);
-console.log(userObj);
+const createError = (msg: string): never => {
+  throw new Error(msg);
+};
 
 function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
   if (isBirthday) {
-    return `Congrats ${userName.toUpperCase()}, age ${age + 1}`;
-  } else {
-    return 'Error ';
+    return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+  } else if (!isBirthday) {
+    return 'Too bad!';
   }
+  return createError('Error');
 }
 
-console.log(
-  logBrtMsg(userObj.isBirthdayData, userObj.userNameData, userObj.ageData)
-);
+console.log(logBrtMsg(isBirthdayData, userNameData, ageData));
---
+++ typescript-01/lessions/index-011.ts
@@ -0,0 +1,18 @@
+// const isBirthdayData: boolean = true;
+// const ageData: number = 40;
+// const userNameData: string = 'John';
+
+// const createError = (msg: string): never => {
+//   throw new Error(msg);
+// };
+
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+//   } else if (!isBirthday) {
+//     return 'Too bad!';
+//   }
+//   return createError('Error');
+// }
+
+// console.log(logBrtMsg(isBirthdayData, userNameData, ageData));
