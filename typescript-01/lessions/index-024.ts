c11db00f94004aa76d5ca606cc6c4fd59eb38e98
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-10-14 10:33:10 +0300

    lesson: add typescript-01 index-024

--- typescript-01/lessions/index-024.ts
+++ typescript-01/lessions/index-024.ts
@@ -54,25 +54,25 @@
 //   left: '50px',
 // };
 
-const serverNewConfig: BasicConfig = {
-  protocol: 'https',
-  port: 3001,
-  // role: 'admin',
-};
+// const serverNewConfig: BasicConfig = {
+//   protocol: 'https',
+//   port: 3001,
+//   // role: 'admin',
+// };
 
-const backupConfig: BasicConfig = {
-  protocol: 'https',
-  port: 3000,
-};
+// const backupConfig: BasicConfig = {
+//   protocol: 'https',
+//   port: 3000,
+// };
 
-interface BasicConfig {
-  protocol: string;
-  port: number;
-}
+// interface BasicConfig {
+//   protocol: string;
+//   port: number;
+// }
 
-const startNewServer = (config: BasicConfig): 'Server started' => {
-  console.log(`Server started on ${config.protocol}://server:${config.port}`);
-  return 'Server started';
-};
-startNewServer(serverNewConfig);
-startNewServer(backupConfig);
+// const startNewServer = (config: BasicConfig): 'Server started' => {
+//   console.log(`Server started on ${config.protocol}://server:${config.port}`);
+//   return 'Server started';
+// };
+// startNewServer(serverNewConfig);
+// startNewServer(backupConfig);
