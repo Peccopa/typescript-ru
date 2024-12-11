345d678699e53ad9ef67510f228fe4b747718c4f
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-12-11 09:03:33 +0300

    feat: add ts-01 arrays

---
+++ typescript-01/features/annotations/arrays.ts
@@ -0,0 +1,16 @@
+const colors2: string[] = ['red', 'green', 'yellow'];
+const dates = [new Date(), new Date()];
+const fruitsByColor = [['tomato'], ['apple'], ['lemon']];
+const fruitsByColor1: string[][] = [['tomato'], ['apple'], ['lemon']];
+
+//Help with inference when extractinf values
+const color2 = colors2[0];
+const color2_2 = colors2.pop();
+
+// Prevent incompatible values
+// colors.push(true);
+
+// Help with 'map'
+colors2.map((color: string): string => {
+  return color.toUpperCase();
+});
--- typescript-01/features/annotations/variables.ts
+++ typescript-01/features/annotations/variables.ts
@@ -21,3 +21,34 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+//functions
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use type annotations
+// 1. Function returns any type
+
+const json = '{"x": 10,"y": 20,}';
+const coords: { x: number; y: number } = JSON.parse(json);
+console.log(coords);
+
+// 2. When we declare a variable on one line and initialize it later
+let words = ['one', 'two', 'three'];
+let isTwo: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'two') {
+    isTwo = true;
+  }
+}
+
+// 3. Variables whoes type can`t be inferred
+let myNumbers = [-10, -1, 12];
+let positiveNumbers: boolean | number = false;
+for (let i = 0; i < myNumbers.length; i++) {
+  if (myNumbers[i] > 0) {
+    positiveNumbers = myNumbers[i];
+  }
+}
